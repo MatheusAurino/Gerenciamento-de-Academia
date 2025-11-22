e9bf5809ce780c5a5b17c1c69fa84cd50a78879c
MatheusAurino <matheusaurino87@gmail.com>
2025-11-22 18:46:31 -0300

    Revert "Login implementado junto com a integração do banco de dados"
    This reverts commit c7198afa9a59849785edddf83fa1d9ee6a80f4b2.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,10 @@
 import br.edu.ifpr.pgua.eic.tads.utils.JavalinUtils;
-import controllers.LoginController; 
 
 public class App {
     public static void main(String[] args) {
 
         var app = JavalinUtils.makeApp(7070);
 
-       
         app.get("/", new controllers.IndexController().get);
-       
-        app.post("/home", new LoginController().autenticar); 
     }
 }
