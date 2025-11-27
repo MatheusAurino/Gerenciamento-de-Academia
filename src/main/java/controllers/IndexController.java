1024172fbd2f2bd201d488e8801813e157d7c226
MatheusAurino <matheusaurino87@gmail.com>
2025-11-27 13:44:39 -0300

    Integração de login e lista de usuarios

---
+++ src/main/java/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler; 
+
+public class IndexController {
+    
+    
+    public Handler get = (Context ctx) -> {
+        ctx.render("index.html");
+    };
+}
